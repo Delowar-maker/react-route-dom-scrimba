7976d5591552343df6ab3789ca7db36cb9320ba4
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-11-07 23:14:28 +0600

    index Route use

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import "../server";
+import HostLayout from "./components/HostLayout";
 import Layout from "./components/Layout";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
 import VanDetail from "./pages/VanDetail";
 import Vans from "./pages/Vans";
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Route>
     </Routes>
   );
